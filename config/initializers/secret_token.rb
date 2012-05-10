de582710182ed1fcd046920b426500e815af2a0f
Clément <clmntlxndr@gmail.com>
2012-05-10 19:58:16 +0200

    Empty Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Texterest::Application.config.secret_token = '8b5045fe236e36f98b15783a4d721df598bed053b69aa4da0de051630d127b8ed0f9e3a2b6bea7eb2b85aa59bed26bde3201d79ffc781c5b84f40978bec91eef'
